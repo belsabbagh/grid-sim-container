76156ad6ac293905ef2f1de1365ea7e34288a30e
Belal Elsabbagh <belsabbagh@gmail.com>
2024-04-02 22:34:12 +0200

    Update paths and formatting in run.py

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import os
-import sys
 import subprocess
-UI_REPO_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "grid-sim-ui-client")
-SERVER_REPO_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "grid-simulator")
+
+pwd = os.getcwd()
+
+UI_REPO_PATH = os.path.join(pwd, "grid-sim-ui-client")
+SERVER_REPO_PATH = os.path.join(pwd, "grid-simulator")
 
 if __name__ == "__main__":
-  subprocess.run(["npm", "install"], cwd=UI_REPO_PATH)
-  subprocess.run(["npm", "run", "build"], cwd=UI_REPO_PATH)
-  subprocess.Popen(["npm", "run", "preview"], cwd=UI_REPO_PATH)
+    subprocess.run(["npm", "install"], cwd=UI_REPO_PATH)
+    subprocess.run(["npm", "run", "build"], cwd=UI_REPO_PATH)
+    subprocess.Popen(["npm", "run", "preview"], cwd=UI_REPO_PATH)
 
-  subprocess.run(["python", "server.py"], cwd=SERVER_REPO_PATH)
+    subprocess.run(["python", "server.py"], cwd=SERVER_REPO_PATH)
