b012e5bf6381ee78e93c57cb3adf4553165e7282
belsabbagh <belsabbagh@gmail.com>
2024-05-06 15:28:08 +0300

    set up poetry to manage env

--- dev.py
+++ dev.py
@@ -7,6 +7,5 @@ UI_REPO_PATH = os.path.join(pwd, "grid-sim-ui-client")
 SERVER_REPO_PATH = os.path.join(pwd, "grid-simulator")
 
 if __name__ == "__main__":
-    subprocess.Popen(["npm", "run", "dev"], cwd=UI_REPO_PATH)
-
-    subprocess.run(["python", "server.py"], cwd=SERVER_REPO_PATH)
+    subprocess.Popen(["python", "server.py"], cwd=SERVER_REPO_PATH)
+    subprocess.run(["npm", "run", "dev"], cwd=UI_REPO_PATH)
